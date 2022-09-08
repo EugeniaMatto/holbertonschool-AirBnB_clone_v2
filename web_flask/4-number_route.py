d467783b802cc8357f45a499deb835cdd6c3ce36
EugeniaMatto <eugematto26@gmail.com>
2022-09-07 18:02:57 -0700

    task 4

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -27,10 +27,9 @@ def indexPy(text="is cool"):
     return ("Python {}".format(text)).replace('_', ' ')
 
 
-@app.route("/number/<n>", strict_slashes=False)
+@app.route("/number/<int:n>", strict_slashes=False)
 def isN(n):
-    if n.isnumeric():
-        return "{} is a number".format(n)
+    return "{} is a number".format(n)
         
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
