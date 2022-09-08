27f9ca3a7f4b87b059680cabc8f53e8a5e859b08
EugeniaMatto <eugematto26@gmail.com>
2022-09-07 17:34:37 -0700

    task 2 fixed

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -18,7 +18,7 @@ def index1():
 
 @app.route("/c/<text>", strict_slashes=False)
 def indexC(text):
-    return "C {}".format(text)
+    return ("C {}".format(text)).replace('_', ' ')
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
