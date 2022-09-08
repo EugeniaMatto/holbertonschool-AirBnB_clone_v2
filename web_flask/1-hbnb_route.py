17c6b8f60393dd3b7fa53f774cb00f102b3112e8
EugeniaMatto <eugematto26@gmail.com>
2022-09-07 17:22:07 -0700

    task 1

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""task 0"""
+"""task 1"""
 
 from flask import Flask
 
@@ -9,6 +9,10 @@ app = Flask(__name__)
 @app.route("/", strict_slashes=False)
 def index():
     return "Hello HBNB!"
+
+@app.route("/hbnb", strict_slashes=False)
+def index1():
+    return "HBNB"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
